5421bb81d32f31eaa71b3033a005c455c350e645
RHUTHER OS <rhuther@os.local>
2026-08-24 21:47:26 -0300

    RHUTHER OS - Fisioterapia: Anatomia 12 capitulos + Quiz 10k niveis + Login ADM + Firebase fisio-e512c + PWA mobile

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase config real - projeto fisio-e512c
+export const firebaseConfig = {
+  apiKey: "AIzaSyAnHVaiO9TBBaaBFybtAllxa19aJep9ar8",
+  authDomain: "fisio-e512c.firebaseapp.com",
+  projectId: "fisio-e512c",
+  storageBucket: "fisio-e512c.firebasestorage.app",
+  messagingSenderId: "172086759308",
+  appId: "1:172086759308:web:743ce5eead603c552fd0a9",
+  measurementId: "G-7VMD5BMB5M"
+};
+// Compat SDK já inicializado no index.html via FirebaseRHUTHER
+// Para modular: import {initializeApp} from 'firebase/app'; initializeApp(firebaseConfig);
